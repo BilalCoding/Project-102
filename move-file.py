3567c628139fd8e715ad8d4df24db8a31ce177d1
BilalCoding <88141297+BilalCoding@users.noreply.github.com>
2023-07-16 22:07:03 +0100

    Add files via upload

---
+++ move-file.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+from_dir = "C:/Users/bilal/Downloads"
+to_dir = "C:/Users/bilal/Desktop/Coding Files/Projects/102"
+
+list_of_files = os.listdir(from_dir)
+# print(list_of_files)
+
+for i in list_of_files:
+    name, ext = os.path.splitext(i)
+    # print(name, ext)
+    if ext == "":
+        continue
+    if ext in [".txt", ".doc", ".docx", ".pdf"]:
+        path1 = from_dir + "/" + i
+        path2 = to_dir + "/Document_Files"
+        path3 = to_dir + "/Document_Files/" + i
+
+        if os.path.exists(path2):
+            print("Moving", i + "...")
+            shutil.move(path1, path3)
+        else:
+            os.makedirs(path2)
+            print("Moving", i + "...")
+            shutil.move(path1, path3)
